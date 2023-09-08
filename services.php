528e5b8a123cd646a861f0fb4747de2734f5aea8
lazydesigner <88834959+lazydesigner@users.noreply.github.com>
2023-09-08 12:42:15 +0530

    Update services.php

--- services.php
+++ services.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://localhost/deepak/assets/css/services.css">
+    <link rel="stylesheet" href="https://shipcartoanotherstate.com/assets/css/services.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.min.css" integrity="sha512-/VYneElp5u4puMaIp/4ibGxlTd2MV3kuUIroR3NSQjS2h9XKQNebRQiyyoQKeiGE9mRdjSCIZf9pb7AVJ8DhCg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Document</title>
 </head>
