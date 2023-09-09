f87b0835f622dd7af92d953ecc3b518a00d13bb3
deepak <deepakbaradwaj933@gmail.com>
2023-09-09 18:13:55 +0530

    navbar

--- navbar.php
+++ navbar.php
@@ -11,7 +11,7 @@
             <div class="formobile">
                 <ul>
                 <li class="logo" style="position: relative;"><a href="https://shipcartoanotherstate.com" style="display: flex;"><span style="position:absolute;opacity: 0;">.</span><img src="https://shipcartoanotherstate.com/assets/images/Modern.webp"
-                                alt="https://shipcartoanotherstate.com" width="100%"  height="auto"></a></li>
+                                alt="https://shipcartoanotherstate.com" width="100%"  height="100%"></a></li>
                 <li><span id="navbar" style="cursor:pointer;"><i class="ri-menu-3-fill" id="navbar2"></i><i class="ri-close-line" id="navbar3"></i></span></li>
                 </ul>
             </div> 
@@ -20,7 +20,7 @@
                 <li><a href="https://shipcartoanotherstate.com">Home</a></li>
                 <li class="services-ul"><a href="services">Services</a> </li>
                 <li class="logo hidelogoforphone" style="position: relative;"><a href="" style="display: flex;"><span style="position:absolute;opacity: 0;">.</span><img src="https://shipcartoanotherstate.com/assets/images/Modern.webp"
-                            alt="" width="100%"></a></li>
+                            alt="" width="100%" height="100%"></a></li>
                 <li><a href="contact">Contact</a></li>
                 <li><a href="blogs">Blogs</a></li>
             </ul>
