bd7b895e3d2de7fd11a18c369bd5da37ccca8b45
deepak <deepakbaradwaj933@gmail.com>
2023-09-12 12:51:57 +0530

    page update

---
+++ privacy-and-policy.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="https://shipcartoanotherstate.com/assets/images/Modern.png" type="image/png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.min.css" integrity="sha512-/VYneElp5u4puMaIp/4ibGxlTd2MV3kuUIroR3NSQjS2h9XKQNebRQiyyoQKeiGE9mRdjSCIZf9pb7AVJ8DhCg==" crossorigin="anonymous" referrerpolicy="no-referrer"  async/>    
+    <link rel="stylesheet" href="./assets/css/index.css">
+    <title>terms-and-condition</title>
+
+    <style>
+        .terms{
+            padding: 5%;
+        }
+        .terms ul{
+            padding: 1% 2%;
+        }
+        p, li{
+            font-size: 1.1rem;
+            color: grey;
+            line-height: 25px;
+        }
+        h1,h2,h3{color: #36454F;}
+    </style>
+
+</head>
+<body>
+    <?php include './navbar.php' ?>
+    <div class="terms">
+        <h1>Ship Car To Another State Privacy Policy</h1>
+        <p>Welcome! Ship Car To Another State  LLC (“Ship Car To Another State ”). We understand the importance of privacy to you and to us. As You are a partner of Ship Car To Another State ’s services, we collect your information and, in some cases, disclose information about you to third parties websites, links and applications. Our privacy policy (“Privacy Policy”) shows you what information we gather from you and how we protect your information. By using the Ship Car To Another State website and the content contained and our services, you consent to all the terms mentioned in the most recent version of our Privacy Policy. You should also read our Terms and conditions to understand the general rules about your use of this Site, and any additional terms that may apply when you access particular services or materials on certain areas of our Site. We mean Ship Car To Another State  and its affiliates. You, a visitor, means the individual accessing this Site and using our services. This Privacy Policy is incorporated into and subject to the terms of the Ship Car To Another State ’s Site Terms of Use.</p>
+        <h2><strong>Minors</strong></h2>
+        <p>Ship Car To Another State is not directed towards children and we do not intentionally collect or maintain Personal Data from any person under the age of 18. IF YOU ARE 18 YEARS OR YOUNGER, PLEASE DO NOT USE OUR SITE OR ANY OF OUR SERVICES FOR ANY PURPOSE AT ANY TIME.</p>
+        <h2>Types of Data we collect</h2>
+        <h3>Types of Data we collect</h3>
+        <p>“Personal Data” is data that identifies an individual or relates to an identifiable individual; We collect Personal Data, such as your name, address, email address, phone number, zip code as it help to ship car to and from location, and other information you provide to us when you book our services, requesting a quote for our services, or interacting with or through the Site.</p>
+        <h3>Your Usage of Our Site</h3>
+        <p>We automatically track some certain information about you based upon how you use the Ship Car To Another State Site. We use this collected information to conduct internal research on our users’ demographics, interests, and behavior to better understand and improve our services for our users. Eventually information may include device information the URL that you just came from, which URL you next go to, what search engine you are using, access times, your IP address, pages viewed, and details about how you’ve used the Site with identifiers associated with cookies or other technologies that may uniquely identify your device or search engine. We may combine this information with information obtained from other sources such as our third-party service providers or websites along with quotes we provide to you to personalize the services.</p>
+        <h3>Your Posts</h3>
+        <p>We offer you to post messages on our Site. If you post messages on our Site, we will collect such information about you as you may choose to disclose your activities on the Site.</p>
+        <h2>Your Communications & Correspondence with our site</h2>
+        <p>If you send us emails, letters or other personal correspondence or if other users or third parties send us correspondence about your activities or postings on the Site, we collect such information into a file specific to you or share them with third parties who provide services to our team. Communications with our customer service agents monitored for quality assurance and to be able to improve services for you.</p>
+        <h2>Your Payment Information</h2>
+        <p>If you establish a credit account or purchase any of our services, we collect your payment information and some additional information, including billing address, credit card number, and credit card expiration date.</p>
+        <h2>Cookies</h2>
+        <p>Like every website we also use cookies but only on some pages of our Site. A cookie is a set of data that is stored on every device that helps websites to remember things about you. Cookies used to track a user’s steps or automatically generate a user’s password. Some features of our Site only be available through the use of a cookie. Cookies can also help us to provide information which can be of your interests. Cookies are always stored on the owner's hard drive, not on our Site. Most cookies are deleted automatically at the end of a session. You can decline our cookies if your browser permits, although your use of the Site is restricted. We use Google Analytics and other data collection tools as it helps us to understand how the Site is used. Customers would like to use of your information by Google analytics, customers use Google’s analytics opt-out browser add-on designed for this purpose sometimes.</p>
+
+        <h2>Why we collect your Information</h2>
+        <h3>To provide you the best services</h3>
+        <p>The data Ship Car To Another State owns is our information related to our business customers. Ship Car To Another State uses the Personal Data to provide our customers our best services, including for our normal operating procedures and internal purposes. If you do not provide the data that we request, or prohibit us from collecting such data, we may not be able to provide you with the requested services.</p>
+        <h3>Administrative Notices</h3>
+        <p>We use customers' email address, customers mailing address, and phone number to contact you regarding administrative and other important notices, new product offerings, and communications relevant to customer use of the Site. If customers do not wish to receive these communications, they opt out of receiving these notices by emailing Ship Car To Another State  at info@ShipCarToAnotherState.com.</p>
+        <h3>Consent</h3>
+        <p>By providing a phone number and clicking submit, customers agree to our Terms of Use and Privacy Policy, and authorize us to make or initiate sales calls, text messages and pre recorded voicemails to that phone number. Customers agreement is not a condition of purchasing any products, goods or services and they opt out at any time or elect to do business with us over the phone. Message & data rates may apply.</p>
+        <h3>Billing and Support</h3>
+        <p>When customers open a credit account through our Site, we use customers address and billing information to bill them and provide associated support.</p>
+        <h3>Dispute Resolution</h3>
+        <p>
+We use information in the file we maintain about customers, and other information we obtain from customers' current and past activities on our Site, to resolve disputes, troubleshoot problems, and enforce our Site Terms of Use.</p>
+
+
+</div>
+    <?php include './footer.php' ?>
+</body>
+</html>
--- terms-and-condition.php
+++ terms-and-condition.php
@@ -3,11 +3,113 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="https://shipcartoanotherstate.com/assets/images/Modern.png" type="image/png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.min.css" integrity="sha512-/VYneElp5u4puMaIp/4ibGxlTd2MV3kuUIroR3NSQjS2h9XKQNebRQiyyoQKeiGE9mRdjSCIZf9pb7AVJ8DhCg==" crossorigin="anonymous" referrerpolicy="no-referrer"  async/>    
+    <link rel="stylesheet" href="./assets/css/index.css">
     <title>terms-and-condition</title>
+
+    <style>
+        .terms{
+            padding: 5%;
+        }
+        .terms ul{
+            padding: 1% 2%;
+        }
+        p, li{
+            font-size: 1.1rem;
+            color: grey;
+            line-height: 25px;
+        }
+        h1,h2,h3{color: #36454F;}
+    </style>
+
 </head>
 <body>
     <?php include './navbar.php' ?>
-    
+    <div class="terms">
+        <h1>Welcome to the Ship Car To Another State!</h1>
+        <p>Our terms and conditions are here to notify you how you should use https://shipcartoanotherstate.com website under community guidelines.
+If you use our website, you have to agree with some of our conditions. If you don't agree with our terms and conditions, you may not be allowed to use the Ship Car To Another State.
+</p>
+<h2><strong>Cookies:</strong></h2>
+<p>Our website uses cookies to give you a personalized experience with our website. You have to agree to use the cookies when you visit our website.
+</p>
+<p>Cookies are the text documents that a web page service puts on a user's hard drive. Cookies cannot send viruses or run programs on user devices.</p>
+<p>To make our website more reliable and advanced, we may use cookies to gather, track, and store information from users for statistical reasons. Users have the option whether you want to accept cookies or not, but there are some cookies that are important for our website to work. These cookies are mandatory; but you don't have to agree with them. Please remember that when you give permission to the cookies of our website, you are also allowed access to third-party websites as well. If a video shows a window that was made by a third party and that was added to our website.</p>
+<h2><strong>Services:</strong></h2>
+<p>On the customer's request Ship Car To Another State arranges the transportation of the customer's shipment by a professional carrier according to these terms.</p>
+<p>When Ship Car To Another State carrier accepts the customer's order, the service considered to be finished.The customer has to know and agree to the our following terms:</p>
+<ul>
+    <li>Act as sole transportation broke</li>
+    <li>Should not be a transporter or motor carrier.</li>
+    <li>Not to represent itself as providing property transportation.</li>
+</ul>
+<h2><strong>License:</strong></h2>
+<p>With Ship Car To Another State experience the full protection of IPR(intellectual property rights) which we provide. You can discover the intellectual rights to all the content in the Ship Car To Another State. Our all rights are reserved. Use all the exclusive service that Ship Car To Another State offers for your personal use but make sure to follow the terms and conditions that are outlined with restrictions.
+</p>
+<h2><strong>Remember not to do these things?</strong></h2>
+<ul>
+    <li>Copy, republish by rephrasing the content authorized by the Ship Car To Another State.</li>
+    <li>Sell, rent, or sub-license material from Ship Car To Another State.</li>
+    <li>Do not further redistribute content from the Ship Car To Another State.</li>
+    <li>This agreement will commence on the current date.</li>
+</ul>
+<p>Some sections of our website may provide the chance to share and discuss opinions and information within a specific area of the website. We are not engaged in filtering, publishing, editing, or reviewing the comments prior to their appearance on the website. The customers express their opinions in comments. Comments are the way to express the perspectives and beliefs of the individual who shares them.
+</p>
+<h2><strong>What your warrant represents?</strong></h2>
+<ul>
+    <li>We provided you the right to post comments on our website with all necessary license and consent to do so</li>
+    <li>Comments are free from any IPR, such as copyright, patent, and belonging to any third party.</li>
+    <li>The users agree to refrain from posting comments that may be libelous, offensive, indecent, or unlawful.</li>
+    <li>Comments should not be used to promote business or engage in other commercial or illegal practices.</li>
+</ul>
+<h2><strong>Responsibility As Our Customers</strong></h2>
+<ul>
+    <li>The information should accurate:</li>
+    <li>Every customer has to provide each and every detail in order to confirm their shipment, bill of lading, and other additional documents, including the vehicle description like year, make, model, style, trim, etc. Alterations and corrections can be a reason to charge a bit extra while shipping.</li>
+    <li>Shipment size and condition:</li>
+    <li>The customer has to be transparent while making a booking about the vehicle's size and condition before pickup. Additional fees may be charged in case of oversized wheels, extra-large tires, or if it is a large vehicle like a limousine.</li>
+    <li>Customers will be responsible for preparing the vehicle:</li>
+    <li>The customer should acknowledge and agree to be responsible for preparing the vehicle for transportation. That means customers should remove all the tags, loose parts, and low-hanging spoilers prior to shipping their vehicle from transportation.</li>
+    <li>Alarm:</li>
+    <li>You should disable all the alarm systems of your vehicle. You have an option to give clear instructions to the carrier how to disengage it. If you didn't disable the alarm system then the carrier may need to use any reasonable means to silence the alarm.</li>
+    <li>Personal stuff:</li>
+    <li>Customers are allowed to put their personal stuff that weighs under 100 pounds, not more than that. It has to be put in the truck or storage area of the vehicle.</li>
+</ul>
+<h2><strong>Fees:</strong></h2>
+<p>When you place your order with the Ship Car To Another State, we have the right to charge non-refundable fees of $100. We divide payment in two parts. You have to pay your first installment (which is usually known as the initial payment) when we assign a carrier to ship your vehicle. The remaining balance is due when your vehicle has been delivered to your desired location.
+</p>
+<h2><strong>Some important points regarding to the payment options are:</strong></h2>
+<ul>
+    <li>Initial payment should be done by credit card or debit card. When you make an order, please include your credit or debit card details. If the initial payment becomes due, the card will be charged.</li>
+    <li>Ensure that after the initial payment, the remaining balance should be paid at the time of delivery. You have both options available for payment via cash, cheque, or credit card.</li>
+    <li>All payments should be done by their due date; fees cannot be disputed for any reason, including if you are dissatisfied with the service or if there are any damages to the vehicle.</li>
+    <li>If you do not make payment on time or if we receive a chargeback for your payment due to any reason, you have to pay extra charges. Interest will also be charged on late payments from the original due date.</li>
+</ul>
+<h2><strong>Fully insured:</strong></h2>
+<p>The  carrier will be responsible if any damage happens to customers' vehicles while shipping. Contracted carriers will be responsible for covering any damages or theft caused by the driver only during transportation</p>
+<p>Carriers are not authorized or covered by insurance to transport personal stuff in the vehicle as insurance policy did not cover personal stuff. We understand that you may need to transport some stuff in the vehicle, but please make sure that the stuff should be in the trunk with a total weight of 100 pounds as more than that will be against transportation policy . Extra charges may be charged if it exceeds the weight of 100 pounds.
+</p>
+<h2><strong>Vehicle Inspection:</strong></h2>
+<p>No client should release or receive the vehicle from the carrier driver without any inspection, regardless of the weather conditions or time of the day. If the client is unable to do the inspection, you cannot file a damage claim. Any damage will be reported within 24 hours of delivery. Each damage has been mentioned in BOL. It is important for a customer to inspect the vehicle carefully.
+    </p>
+    <h2><strong>Governing Law   </strong></h2>
+    <p>Clients have the opportunity to seek legal counsel for advice. This agreement shall not be subjected to any risk of construction that ambiguities against the drafting party.</p>
+    <p>The state law shall govern this agreement. Both parties have to mutually agree that every legal action related to this agreement will be filed in a court within the state.
+    </p>
+    <h2><strong>Contact information:    </strong></h2>
+    <p>For any requirements and further questions about the terms and conditions of the service should be sent to us at info@shipcartoanotherstate.com
+    </p>
+
+
+
+
+
+
+
+
+
+</div>
     <?php include './footer.php' ?>
 </body>
 </html>
